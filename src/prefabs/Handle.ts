1bcd773e3abf594deb2351d53818ca0550d4d9ea
Icaka <ico.ezekiev@gmail.com>
2024-10-14 15:35:39 +0300

    Handle rotation with gsap

--- src/prefabs/Handle.ts
+++ src/prefabs/Handle.ts
@@ -1,5 +1,8 @@
 import { Container, Sprite } from "pixi.js";
 import { centerObjects } from "../utils/misc";
+import gsap from "gsap";
+import PixiPlugin from "gsap/PixiPlugin";
+import * as PIXI from "pixi.js";
 
 
 export default class Door extends Container {
@@ -8,6 +11,7 @@ export default class Door extends Container {
     // private _sprite: Sprite;
     private _handle: Sprite;
     private _handleShadow: Sprite;
+    private handleRotation = 0;
     constructor(
         protected handl: string,
         protected shadow: string,
@@ -27,12 +31,14 @@ export default class Door extends Container {
         this.addChild(this._handle);
 
         this.positionHandle();
+
+        gsap.registerPlugin(PixiPlugin);
+        PixiPlugin.registerPIXI(PIXI);
+        this.eventMode = 'static';
+        this.on('pointerdown', this.onClick);
     }
 
     resize(width: number, scFactor: number) {
-        // this._sprite.anchor.set(0);
-
-        //this._scaleFactor = window.innerHeight / this._sprite.texture.height;
 
         this._handle.width = width / scFactor;
 
@@ -45,5 +51,14 @@ export default class Door extends Container {
     positionHandle() {
         centerObjects(this);
         this.x -= this.x * (1/37);
+    }
+
+    onClick() {
+        console.log("clicked handle");
+        this.handleRotation += 30;
+        gsap.to(this._handle, {
+            pixi: {rotation: this.handleRotation },
+            duration: 1,
+          });
     }
 }
